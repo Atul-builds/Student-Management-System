e84042e07adc30dea652b63b97d134980bfe9492
Atul <atulshukla31215@gmail.com>
2026-09-10 10:51:42 +0530

    Add files via upload

---
+++ Student_Grade_Calculator.py
@@ -0,0 +1,32 @@
+
+marks = float(input("Enter Your marks :"))
+
+
+if marks >=500:
+    print("Enter Your Right Marks") 
+    
+elif marks >=400:
+    print("This is Your Percentage ", marks / 500*100,)
+    print("Grate = A","-> Pass!")
+
+elif marks >=300:
+    
+    print("This is Your Percentage", marks / 500*100,)
+    print("Grate = B","-> Pass!")
+
+elif marks >=200:
+    
+    print("This is Your Percentage", marks / 500*100,)
+    print("Grate = C","-> Pass!")
+
+
+elif marks >=164:
+    
+    print("This is Your Percentage", marks / 500*100,)
+    print("Grate = D","-> Pass!")
+
+elif marks >=0:
+    
+    print("This is Your Percentage", marks / 500*100,)
+    print("Grate = E" ,"-> Fail!")
+
